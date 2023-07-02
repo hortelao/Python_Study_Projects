7b7f24885b0080f94fb7d120401acf79c03974c0
André Hortelão <andrefhortelao@gmail.com>
2023-07-02 20:46:31 +0100

    Refactoring
    Added method try catch except for error handling on case the user enter a non alphabetic character. Added function to restart the prompt in case of error

--- 26 - NATO-alphabet/main.py
+++ 26 - NATO-alphabet/main.py
@@ -1,11 +1,17 @@
 import pandas
 nato_data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
-user_input = input("What to transform? ").upper()
-
-user_letters = [letter for letter in user_input]
 
 nato = {row.letter:row.code for (index, row) in nato_data_frame.iterrows()}
 
-nato_final = [nato[letter] for letter in user_letters]
-print(nato_final)
 
+def generate():
+    user_input = input("What to transform? ").upper()
+    try:
+        nato_final = [nato[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please... (No special characters or numbers)")
+        generate()
+    else:
+        print(nato_final)
+
+generate()
